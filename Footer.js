6a56df7da578d4d4cf35dffe7a80cd6879bbaddd
Ali Fay <ali.fay@gmail.com>
2020-12-28 10:44:35 +0000

    Added scrubbed Footer.js

---
+++ Footer.js
@@ -0,0 +1,18 @@
+
+
+function Footer() {
+    return (
+        <div className="footer">
+        <div className="containermain">
+            <div>VISIT</div>
+            <div className="contact">CONTACT</div>
+            <div className="follow">FOLLOW</div>
+        </div>
+        <div className="container2">
+            <a href="https://www.linkedin.com/in/hoisan-ng-0025aa114" target="_blank" rel="noreferrer">Linkedin</a>
+            <a href="https://github.com/Hoisanng" target="_blank" rel="noreferrer">Github</a>
+        </div>
+        </div>
+    )
+}
+export default Footer;
